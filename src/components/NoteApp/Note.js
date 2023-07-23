c2f40c856788400d2138a9641fe9d5a3776a468c
hesam <m.hesam.moradian@gmail.com>
2023-07-23 18:24:28 +0330

    finish

--- src/components/NoteApp/Note.js
+++ src/components/NoteApp/Note.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Note extends Component {
-
+let 
     render() {
+                const { id, title, color } = this.props.obj;
+
         return (
-            <div className="card shadow-sm rounded" style={{ backgroundColor: '#fff' }}><p className="card-text p-3">Go To Gym</p></div>
+            <div className="card shadow-sm rounded" style={{ backgroundColor: color }}><p className="card-text p-3">{ title}</p></div>
         )
     }
 }
